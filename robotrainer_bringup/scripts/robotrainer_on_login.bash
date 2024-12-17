c1431bfcd3ed0b2cbf0ba33a45921c2b2dbf7b4e
Andreas Zachariae <andreas.zachariae@gmail.com>
2024-12-17 17:13:40 +0100

    Change autostart to angular 3 and no rviz

--- robotrainer_bringup/scripts/robotrainer_on_login.bash
+++ robotrainer_bringup/scripts/robotrainer_on_login.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 ## Update to default kinematics
-rosservice call /robotrainer_hw/set_state "{angular_key: 5, linear_key: 1, without_controller_updates: True}"
+rosservice call /robotrainer_hw/set_state "{angular_key: 3, linear_key: 1, without_controller_updates: True}"
 
-konsole -e "roslaunch robotrainer_bringup sr3_on_login.launch" &!
+roslaunch robotrainer_bringup sr3_on_login.launch
 
-konsole --new-tab -e "rviz --fullscreen -d `rospack find robotrainer_bringup`/configs/rt2_display_fullscreen.rviz" &!
+
+## Original file
+# rosservice call /robotrainer_hw/set_state "{angular_key: 5, linear_key: 1, without_controller_updates: True}"
+# konsole -e "roslaunch robotrainer_bringup sr3_on_login.launch" &!
+# konsole --new-tab -e "rviz --fullscreen -d `rospack find robotrainer_bringup`/configs/rt2_display_fullscreen.rviz" &!
 
